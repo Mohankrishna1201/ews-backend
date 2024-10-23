c07153556fafad6b74be2a7c5c5ee1022f3ad58a
Kattamuri Mohan Krishna <121288107+Mohankrishna1201@users.noreply.github.com>
2024-10-24 00:07:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/send-notification', async (req, res) => {
 });
 
 // Start the Express server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
