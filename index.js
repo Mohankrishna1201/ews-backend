80f7228b571fa80d9c1ffd996ef58dac3c87a5f8
Mohankrishna1201 <21EE01037@iitbbs.ac.in>
2025-01-08 18:49:14 +0530

    added socket

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const admin = require('firebase-admin');
+const { socketHandler } = require("./socketController/SocketFile.js")
 // Initialize Firebase Admin SDK
-const serviceAccount = require('/app/credentials.json');
+const serviceAccount = require('/app/credentials');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 module.exports = admin;
 const app = express();
 app.use(express.json());
-
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 const token = require('./routes/saveToken.js');
 app.use(token);
-
+app.get('/', (req, res) => {
+    res.send("hello");
+})
 const sendMsg = require('./routes/sendMsg.js')
 app.use(sendMsg);
-
+io.on("connection", socketHandler)
 
 // Start the Express server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT)
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
---
+++ socketController/SocketFile.js
@@ -0,0 +1,16 @@
+
+const onConnect = () => { }
+const onTEst = (socket) => {
+    console.log("onTEst", socket.id)
+    socket.emit("test-response", "Emitted Succesfully")
+}
+const onDataRecieved = (socket, data) => {
+    console.log(data, socket.id)
+}
+function socketHandler(socket) {
+    socket.on("connection", onConnect);
+    socket.on("test", () => onTEst(socket))
+    socket.on("data-recieved", (data) => onDataRecieved(socket, data))
+}
+
+module.exports = { socketHandler }
